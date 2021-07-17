00c1b740861df61d25a3ae3462db45f39eb47496
sanjay <sanssitk@gmail.com>
2021-07-16 23:44:06 -0500

    added user input to chrome storage

--- js/app.js
+++ js/app.js
@@ -5,10 +5,38 @@ addItemForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let itemText = addItemForm.elements.namedItem("itemText").value;
     if (itemText) {
+        add(itemText);
         renderActionItem(itemText);
         addItemForm.elements.namedItem("itemText").value = "";
     }
 })
+
+const add = (text) => {
+    let actionItem = {
+        id: 1,
+        added: new Date().toString(),
+        text: text,
+        completed: null,
+    }
+
+
+    chrome.storage.sync.get(["actionItems"], (data) => {
+        let items = data.actionItems;
+        if (!items) {
+            items = [actionItem]
+        } else {
+            items.push(actionItem)
+        }
+        chrome.storage.sync.set({
+            actionItems: items
+        }, () => {
+            chrome.storage.sync.get(["actionItems"], data => {
+                console.log(data)
+            })
+        });
+    });
+
+}
 
 const renderActionItem = (text) => {
 
@@ -35,7 +63,8 @@ const renderActionItem = (text) => {
     item.appendChild(itemDelete);
     element.appendChild(shortLink);
 
-    itemsList.prepend(element)
+
+
 }
 
 const deleteList = (e) => {
