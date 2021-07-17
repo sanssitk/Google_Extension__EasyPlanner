c54a3aad7a3425168232bc327c60e0a9d5d80f7c
sanjay <sanssitk@gmail.com>
2021-07-17 00:12:02 -0500

    displayed user input form chrome storage

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ addItemForm.addEventListener("submit", (e) => {
     let itemText = addItemForm.elements.namedItem("itemText").value;
     if (itemText) {
         add(itemText);
-        renderActionItem(itemText);
         addItemForm.elements.namedItem("itemText").value = "";
     }
 })
@@ -18,8 +17,6 @@ const add = (text) => {
         text: text,
         completed: null,
     }
-
-
     chrome.storage.sync.get(["actionItems"], (data) => {
         let items = data.actionItems;
         if (!items) {
@@ -31,42 +28,37 @@ const add = (text) => {
             actionItems: items
         }, () => {
             chrome.storage.sync.get(["actionItems"], data => {
-                console.log(data)
+                renderActionItem(data);
             })
         });
     });
 
 }
 
-const renderActionItem = (text) => {
+const renderActionItem = (datas) => {
+    datas.actionItems.map(data => {
+        let element = document.createElement("div");
+        element.classList.add("actionItem_container");
+        let item = document.createElement("div");
+        item.classList.add("actionItem_item");
+        let checkBox = document.createElement("div");
+        checkBox.classList.add("action_item_checkBox");
+        let content = document.createElement("div");
+        content.classList.add("action_item_content");
+        let itemDelete = document.createElement("div");
+        itemDelete.classList.add("action_item_delete");
+        let shortLink = document.createElement("div");
+        shortLink.classList.add("actionItem_shortLink");
+        checkBox.innerHTML = `<i class="fas fa-check"></i>`
+        content.textContent = data.text;
+        itemDelete.innerHTML = `<i class="fas fa-times"></i>`
+        shortLink.innerHTML = `<a href="">Four useFull JavaScript shorthands..</a>`
 
-    let element = document.createElement("div");
-    element.classList.add("actionItem_container");
-    let item = document.createElement("div");
-    item.classList.add("actionItem_item");
-    let checkBox = document.createElement("div");
-    checkBox.classList.add("action_item_checkBox");
-    let content = document.createElement("div");
-    content.classList.add("action_item_content");
-    let itemDelete = document.createElement("div");
-    itemDelete.classList.add("action_item_delete");
-    let shortLink = document.createElement("div");
-    shortLink.classList.add("actionItem_shortLink");
-    checkBox.innerHTML = `<i class="fas fa-check"></i>`
-    content.textContent = text;
-    itemDelete.innerHTML = `<i class="fas fa-times"></i>`
-    shortLink.innerHTML = `<a href="">Four useFull JavaScript shorthands..</a>`
-
-    element.appendChild(item);
-    item.appendChild(checkBox);
-    item.appendChild(content);
-    item.appendChild(itemDelete);
-    element.appendChild(shortLink);
-
-
-
-}
-
-const deleteList = (e) => {
-    console.log(e)
+        element.appendChild(item);
+        item.appendChild(checkBox);
+        item.appendChild(content);
+        item.appendChild(itemDelete);
+        element.appendChild(shortLink);
+        document.querySelector(".actionItem_items").prepend(element)
+    })
 }
