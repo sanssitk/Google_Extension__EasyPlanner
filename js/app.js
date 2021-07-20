8cf4aecc26644d068abddfe3afd0fa51556f646d
sanjay <sanssitk@gmail.com>
2021-07-19 21:00:03 -0500

    dynamic time and url link added

--- js/app.js
+++ js/app.js
@@ -4,12 +4,40 @@ let itemsList = document.querySelector(".actionItem_items");
 
 let actionItemsUtils = new ActionItems();
 
+
+// let timeContainer = document.querySelector(".info_container_time");
+// let timeBlock = timeContainer.createElement("span");
+// timeContainer.appendChild()
+
+const getTime = () => {
+    let date = new Date();
+    let hrs = date.getHours();
+    let min = date.getMinutes();
+    let day = date.getDay();
+    let month = date.getMonth();
+    let today = date.getDate();
+    let ampm = hrs >= 12 ? "pm" : "am";
+    hrs = hrs % 12;
+    hrs = hrs ? hrs : 12;
+    let timeCont = document.querySelector(".info_container_time");
+    var time = `${hrs}:${min} ${ampm}`
+    timeCont.append(time)
+    const days = ["Sunday", "Monday", "Tuesday", "WednesDay", "Thursday", "Friday", "Saturday"];
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    dayName = days[day]
+    monthName = monthNames[month]
+    document.querySelector(".info_container_day").innerText = dayName;
+    document.querySelector(".info_container_date").innerText = monthName + " " + today;
+    return timeCont;
+}
+
 //storage.clear();
 storage.get(["actionItems"], (data) => {
+    getTime();
     let items = data.actionItems;
-    console.log(items)
     createQuickActionListener();
     renderActionItems(items)
+    actionItemsUtils.setProgress();
     chrome.storage.onChanged.addListener(() => {
         actionItemsUtils.setProgress();
     });
@@ -17,28 +45,7 @@ storage.get(["actionItems"], (data) => {
 
 const renderActionItems = (items) => {
     items.map(item => {
-        renderActionItem(item.text, item.id, item.completed)
-    })
-}
-const handleQuickActionListener = (e) => {
-    const text = e.target.getAttribute("data-text");
-    const id = e.target.getAttribute("data-id");
-
-    getCurrentTabl().then((tab) => {
-        actionItemsUtils.addQuickActionItem(id, text, tab, (actionItem) => {
-            renderActionItem(actionItem.text, actionItem.id, actionItem.completed)
-        })
-    })
-    // actionItemsUtils.add(text, (actionItem) => {
-    //     renderActionItem(actionItem.text, actionItem.id, actionItem.completed)
-    // });
-
-}
-
-const createQuickActionListener = () => {
-    let buttons = document.querySelectorAll(".quickButton")
-    buttons.forEach((button) => {
-        button.addEventListener("click", handleQuickActionListener)
+        renderActionItem(item.text, item.id, item.completed, item.website)
     })
 }
 
@@ -54,12 +61,30 @@ async function getCurrentTabl() {
     })
 }
 
+const handleQuickActionListener = (e) => {
+    const text = e.target.getAttribute("data-text");
+    const id = e.target.getAttribute("data-id");
+
+    getCurrentTabl().then((tab) => {
+        actionItemsUtils.addQuickActionItem(id, text, tab, (actionItem) => {
+            renderActionItem(actionItem.text, actionItem.id, actionItem.completed, actionItem.website)
+        })
+    })
+}
+
+const createQuickActionListener = () => {
+    let buttons = document.querySelectorAll(".quickButton")
+    buttons.forEach((button) => {
+        button.addEventListener("click", handleQuickActionListener)
+    })
+}
+
 addItemForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let itemText = addItemForm.elements.namedItem("itemText").value;
     if (itemText) {
         actionItemsUtils.add(itemText, null, (actionItem) => {
-            renderActionItem(actionItem.text, actionItem.id, actionItem.completed)
+            renderActionItem(actionItem.text, actionItem.id, actionItem.completed, actionItem.website)
             addItemForm.elements.namedItem("itemText").value = "";
         });
     }
@@ -85,7 +110,7 @@ const handelDeleteClicked = (e) => {
     }
 }
 
-const renderActionItem = (text, id, completed) => {
+const renderActionItem = (text, id, completed, website = null) => {
     let element = document.createElement("div");
     element.classList.add("actionItem_container");
     let item = document.createElement("div");
@@ -98,18 +123,36 @@ const renderActionItem = (text, id, completed) => {
     let itemDelete = document.createElement("div");
     itemDelete.classList.add("action_item_delete");
     itemDelete.addEventListener("click", handelDeleteClicked)
-    let shortLink = document.createElement("div");
-    shortLink.classList.add("actionItem_shortLink");
     checkBox.innerHTML = `<i class="fas fa-check"></i>`
     content.textContent = text;
     itemDelete.innerHTML = `<i class="fas fa-times"></i>`
-    shortLink.innerHTML = `<a href="">Four useFull JavaScript shorthands..</a>`
     if (completed) element.classList.toggle("completed");
-    element.setAttribute("id", id)
-    element.appendChild(item);
+
     item.appendChild(checkBox);
     item.appendChild(content);
     item.appendChild(itemDelete);
-    element.appendChild(shortLink);
+    element.setAttribute("id", id)
+    element.appendChild(item);
+
+    if (website) {
+        let linkContainer = createLinkContainer(website.url, website.fav_icon, website.title);
+        element.appendChild(linkContainer);
+    }
+
     document.querySelector(".actionItem_items").prepend(element)
+}
+
+const createLinkContainer = (url, fav_icon, title) => {
+    let mainLinkEle = document.createElement("div");
+    mainLinkEle.classList.add("actionItem_shortLink");
+    mainLinkEle.innerHTML = `
+        <a href="${url}" target="_blank">
+            <div class="actionItem_favIcon">
+                <img src="${fav_icon}" alt="">
+            </div>
+            <div class="actionItem_title">
+                <span>${title}</span>
+            </div>
+        </a> `
+    return mainLinkEle;
 }
