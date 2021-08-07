323cb4a0533d687aabe1ec4efb6ab77f8b8ba633
sanjay <sanssitk@gmail.com>
2021-08-06 21:55:38 -0500

    firebase config setup

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,6 @@
 let user;
 let userUid = null;
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB7vK6OzqkJqREIE6HVqQv02D2byocsC0I",
-    authDomain: "chrome-todolist-ext.firebaseapp.com",
-    projectId: "chrome-todolist-ext",
-    storageBucket: "chrome-todolist-ext.appspot.com",
-    messagingSenderId: "872114912505",
-    appId: "1:872114912505:web:1837b5ffd688dbe755f628"
-};
-
 // Initializing firebase with config
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore().collection("actionItems");
